fad09c1e7de9aeec238ccb0c41125cf66a9d9bc2
Your Name <you@example.com>
2017-01-27 14:19:05 +0530

    proper indent

--- assets/js/functionality/angular_component.js
+++ assets/js/functionality/angular_component.js
@@ -2,7 +2,7 @@ angular.module('mod1',[]).controller('invoiceController',function invoiceControl
 	this.qty = 5;
 this.price = 2;
 	this.curr = 'EUR';
-	this.currencies =['EUR','USD','CNY']
+	this.currencies =['EUR','USD','CNY'];
 	this.REtoCurrencies ={
 	RE:1,
 	EUR: 100,
@@ -11,14 +11,14 @@ this.price = 2;
 
 this.total = function totalprice(outcurr){
 	return this.convertCurrency(this.qty * this.price, this.inCurr, outcurr);
-}
+};
 
 this.convertCurrency = function convertCurrency(amount, incurr,outCurr ){
 	return amount* this.currencies[incurr]/this.currencies[outCurr];
-}
+};
 
 this.pay = function paid(){
 	alert('Thank you , Visit again!!');
-}
+};
 
 });
