3b4e15da3a476b0aeb77bd0988b4f752a83a1c6c
Your Name <you@example.com>
2017-01-27 15:13:54 +0530

    more formatting

--- assets/js/functionality/angular_component.js
+++ assets/js/functionality/angular_component.js
@@ -1,24 +1,24 @@
-angular.module('mod1',[]).controller('invoiceController',function invoiceController(){
+angular.module('mod1', []).controller('invoiceController', function invoiceController() {
 	this.qty = 5;
-this.price = 2;
+	this.price = 2;
 	this.curr = 'EUR';
-	this.currencies =['EUR','USD','CNY'];
-	this.REtoCurrencies ={
-	RE:1,
-	EUR: 100,
-	USD: 70.12
+	this.currencies = ['EUR', 'USD', 'CNY'];
+	this.REtoCurrencies = {
+		RE: 1,
+		EUR: 100,
+		USD: 70.12
 	};
 
-this.total = function totalprice(outcurr){
-	return this.convertCurrency(this.qty * this.price, this.inCurr, outcurr);
-};
+	this.total = function totalprice(outcurr) {
+		return this.convertCurrency(this.qty * this.price, this.inCurr, outcurr);
+	};
 
-this.convertCurrency = function convertCurrency(amount, incurr,outCurr ){
-	return amount* this.currencies[incurr]/this.currencies[outCurr];
-};
+	this.convertCurrency = function convertCurrency(amount, incurr, outCurr) {
+		return amount * this.currencies[outCurr] / this.currencies[incurr];
+	};
 
-this.pay = function paid(){
-	alert('Thank you , Visit again!!');
-};
+	this.pay = function paid() {
+		alert('Thank you , Visit again!!');
+	};
 
 });
